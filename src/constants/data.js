21ed91488bac8e1caf8bb6c8abbed736bb14148f
WaiYanMoeMyint <wymoemyint4523@gmail.com>
2022-09-04 21:26:23 +0630

    Update data file

---
+++ src/constants/data.js
@@ -0,0 +1,52 @@
+import {HiPencil,BiDevices, RiStackFill} from "react-icons/fa";
+
+export const navLinks = [
+     {
+        id:1,
+        name:"Home",
+        title: "home"
+     },
+     {
+        id:2,
+        name:"Pricing",
+        title: "pricing"
+     },
+     {
+        id:3,
+        name:"Blog",
+        title: "blog"
+     },
+     {
+        id:4,
+        name:"Support",
+        title: "support"
+     },
+     {
+        id:5,
+        name:"Contact",
+        title: "contact"
+     },
+]
+
+export const products  = [
+   {
+      id:1,
+      title:"Easy to use",
+      description:"Editing and customizing Essential Landing is easy and fast.",
+      icon:"HiPencil"
+
+   },
+   {
+      id:2,
+      title:"300+ blocks",
+      description:"Editing and customizing Essential Landing is easy and fast.",
+      icon:"BiDevices",
+   },
+   {
+
+      id:3,
+      title:"100% Responsive",
+      description:"Editing and customizing Essential Landing is easy and fast.",
+      icon:"RiStackFill"
+   },
+];
